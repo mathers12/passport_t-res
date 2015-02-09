9dc6498822c1d4b72be50a85624993c3783364ff
Gustav Hlavac <gustav.hlavac@gmail.com>
2015-02-09 16:59:05 +0100

    smtp config cahnges

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,8 @@ var smtpTransport = nodemailer.createTransport("SMTP",{
     //secureConnection: true, // use SSL
     //port: 587, // port for secure SMTP
     auth: {
-        user: "dsoft.tesla@gmail.com",
-        pass: "something001"
+        user: "info@publishart.eu",
+        pass: "2714info2517"
     }
 });
 
@@ -28,7 +28,7 @@ function sendEmail (email,link,meno,priezvisko)
 {
 
     smtpTransport.sendMail({  //email options
-        from: "dSoft Solutions s.r.o<dsoft.tesla@gmail.com>",
+        from: "dSoft Solutions s.r.o <info@publishart.eu>",
         to: email,
         subject: "Prosím, potvrďte tento e-mail!",
         html: "Dobrý deň pán <b>"+meno+" "+priezvisko+"</b><br><br>Prosím potvrďte tento verifikačný e-mail!<br><a href="+link+">Potvrdiť kliknutím tu!</a>"
